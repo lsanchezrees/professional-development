5e2dae08f5c22af5a269c6ce68df7dfd8d50cefb
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-29 14:37:07 -0600

    created index.php page for professional devt website

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>s
+	<html>
+<head>
+	<title>Professional Development Homework</title>
+</head>
+<body>
+	<h1>Professional Development Homework</h1>
+	<h2>Week 1</h2>
+	<p>My partner is Godfrey Davis</p>
+
+	<p>According to the quiz, my learning style is visual.  However, it was only 45% visual, and auditory was 40%, so that's clearly also a factor for me.</p>
+
+	<p>3 Quantitative Accomplishments I Have Done</p>
+	<ol><li>I have worked successfully as a systems analyst, incorporating programming, database design, data warehousing and systems design.</li>
+		<li>I have completed a web design certificate.</li>
+		<li>I am a licensed acupuncturist/DOM.</li></ol>
+	<p>Qualitative Features About Myself</p>
+	<ol><li>I work well with customers and am very focused on helping customers</li>
+		<li>I can easily handle multiple tasks.</li>
+		<li>I want everyone to succeed and enjoy helping others</li></ol>
+
+
+	<p>Why:  Help a company get their message across more successfully, better</p>
+	<p>How:    Study coding and website design</p>
+	<p>What:  Great websites</p>
+
+	<p>Elevator pitch:  Hi, my name is Leonora.  I want to help companies be more successful by creating great websites for them that are beautiful, easy to use and clear and make them more successful.  I’m doing this by learning all aspects of web coding, including Php, JavaScript, and HTML/CSS.</p>
+
+	<h2>Week 2</h2>
+	<p>I already submitted my resume and discussed it with you.</p>
+	<p>I re-joined LinkedIn and also joined Quora</p>
+	<p>I googled Godfrey, and found one social media platform:  LinkedIn.  I had to google the states with his name to get results, because there are quite a few Godfrey Davises.  I googled his name with New York and then with Albuquerque.  I emailed him the results and he told me he was listed under aliases. He said he is careful about what he puts out on social media.</p>
+	<p>I’m in the process of designing custom business cards.</p>
+	<p>Last one is now complete.</p>
+
+
+</body>
+</html>
